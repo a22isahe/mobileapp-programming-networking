08a04d4b18119c85aa6d488a807f38614257bc0a
a22isahe <a22isahe@student.his.se>
2023-04-27 11:32:34 +0200

    Added JSON_FILE to onPostExecute. adds data to Mountain arraylist from JSON_FILE

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -8,25 +8,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
-   public ArrayList<Mountain> items = new ArrayList<>(Arrays.asList(
+   public ArrayList<Mountain> items;/* = new ArrayList<>(Arrays.asList(
             new Mountain("Matterhorn"),
             new Mountain("Mont Blanc"),
             new Mountain("Denali")
     ));
+*/
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         new JsonFile(this, this).execute(JSON_FILE);
 
+
+      //  Type type = new TypeToken<ArrayList<Mountain>>() {}.getType();
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
             @Override
             public void onClick(Mountain item) {
@@ -44,6 +54,11 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Mountain>>() {}.getType();
+        items = gson.fromJson(json,type);
+
+
     }
 
 }
